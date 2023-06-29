9d35ae972b327632abfa6d480fa8a4d1aa8cdf7e
Nitro1000 <47580067+Nitro1000@users.noreply.github.com>
2023-06-29 02:23:47 +0100

    probando config de tink

--- app/src/main/java/com/example/datatransferbluetooth/BluetoothController.kt
+++ app/src/main/java/com/example/datatransferbluetooth/BluetoothController.kt
@@ -8,18 +8,23 @@ import android.content.Context
 import android.util.Log
 import android.widget.Toast
 import com.google.crypto.tink.Aead
-import com.google.crypto.tink.AeadFactory
+import com.google.crypto.tink.CleartextKeysetHandle
+import com.google.crypto.tink.JsonKeysetReader
+import com.google.crypto.tink.JsonKeysetWriter
 import com.google.crypto.tink.KeysetHandle
+import com.google.crypto.tink.aead.AeadConfig
+import com.google.crypto.tink.aead.AeadFactory
 import com.google.crypto.tink.aead.AeadKeyTemplates
-import com.google.crypto.tink.config.TinkConfig
-import com.google.crypto.tink.integration.android.AndroidKeysetManager
-import java.security.GeneralSecurityException
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import java.nio.ByteBuffer
+import java.nio.charset.Charset
 import java.util.UUID
 import kotlin.math.min
+
 
 interface BluetoothConnectionListener {
     fun onConnectionSuccess()
@@ -57,7 +62,7 @@ class BluetoothController(
     private var bluetoothDataListener: BluetoothDataListener? = null
 
     // Conjunto de claves para cifrar y descifrar los datos
-//    private var keysetHandle: KeysetHandle
+    private var keysetHandle: KeysetHandle
 
     // Instancia de AEAD para cifrar y descifrar los datos
     private var aead: Aead
@@ -67,28 +72,11 @@ class BluetoothController(
         updatePairedDevices()
 
         // Registra la configuración de Tink
-        TinkConfig.register()
+        AeadConfig.register()
+        keysetHandle = KeysetHandle.generateNew(AeadKeyTemplates.AES256_GCM)
+        aead = AeadFactory.getPrimitive(keysetHandle)
 
-        // Genera o carga el conjunto de claves en función de si ya existe o no
-        val keysetManager = AndroidKeysetManager.Builder()
-            .withSharedPref(context, "keyset_prefs", "keyset")
-            .withKeyTemplate(AeadKeyTemplates.AES256_GCM)
-            .withMasterKeyUri("android-keystore://keyset_id")
-            .build()
 
-        if (!keysetManager.isKeysetLoaded) {
-            keysetManager.generateNewMasterKey()
-        }
-
-        try {
-            keysetManager.getPrimitive("android-keystore://keyset_id", Aead::class.java)?.let { primitive ->
-                aead = primitive
-            } ?: throw GeneralSecurityException("Error loading keyset")
-        } catch (e: GeneralSecurityException) {
-            // Manejar el error en caso de que ocurra
-            aead = AeadFactory.getPrimitive(AeadKeyTemplates.AES256_GCM)
-            Log.e("Bluetooth", "Error al cargar el conjunto de claves: ${e.message}")
-        }
     }
 
     // método para actualizar la lista de dispositivos Bluetooth emparejados.
@@ -128,6 +116,19 @@ class BluetoothController(
                     val bufferSize = 4096 // Tamaño del buffer en bytes
                     while (true) {
                         try {
+                            // Leer el tamaño del keysetString que pasó el cliente
+                            val keysetStringSizeBuffer = ByteArray(4)
+                            bluetoothClientSocket?.inputStream?.read(keysetStringSizeBuffer)
+                            val keysetStringSize = ByteBuffer.wrap(keysetStringSizeBuffer).int
+                            // Leer el keysetString que pasó el cliente
+                            val keysetStringBuffer = ByteArray(keysetStringSize)
+                            bluetoothClientSocket?.inputStream?.read(keysetStringBuffer)
+                            val keysetString = String(keysetStringBuffer)
+
+                            val inputStream = ByteArrayInputStream(keysetString.toByteArray(Charset.defaultCharset()))
+                            val receivedKeysetHandle = CleartextKeysetHandle.read(JsonKeysetReader.withInputStream(inputStream))
+
+
                             // Leer el tamaño nombre del archivo
                             val fileNameSizeBuffer = ByteArray(4)
                             bluetoothClientSocket?.inputStream?.read(fileNameSizeBuffer)
@@ -150,8 +151,10 @@ class BluetoothController(
                                     break
                                 }
                                 remainingBytes -= bytes
+                                // Descifrar los datos recibidos
+                                val decryptedData = receivedKeysetHandle.getPrimitive(Aead::class.java).decrypt(fileBuffer, null)
                                 // Construir y guardar el archivo con los datos recibidos
-                                saveFile(context, fileBuffer, fileName, bytes)
+                                saveFile(context, decryptedData, fileName, bytes)
 //                                saveFile(context, decryptedData, fileName, decryptedData.size)
                             }
                         } catch (e: IOException) {
@@ -206,6 +209,15 @@ class BluetoothController(
     fun sendData(data: ByteArray, fileName: String) {
         try {
             bluetoothClientSocket?.outputStream?.apply {
+                val outputStream = ByteArrayOutputStream()
+                CleartextKeysetHandle.write(keysetHandle, JsonKeysetWriter.withOutputStream(outputStream))
+                val keysetString = String(outputStream.toByteArray(), Charset.defaultCharset())
+
+                // Enviar el tamaño del keysetString
+                write(ByteBuffer.allocate(4).putInt(keysetString.toByteArray().size).array())
+                // Enviar el keysetString
+                write(keysetString.toByteArray())
+
                 // mostrar el nombre del archivo que se está enviando
                 bluetoothDataListener?.onDataReceived("Enviando archivo: $fileName")
 
@@ -217,12 +229,21 @@ class BluetoothController(
 
                 // Enviar el tamaño del archivo
                 write(ByteBuffer.allocate(4).putInt(data.size).array())
-                write(data) // Enviar el archivo
+                // cifrar data antes de enviar
+                val encryptedData = encryptData(data)
+                write(encryptedData) // Enviar el archivo
             }
         } catch (e: IOException) {
             Log.e("Bluetooth", "Error al enviar los datos en sendData", e)
         }
     }
+
+    private fun encryptData(data: ByteArray): ByteArray {
+        val aead = keysetHandle.getPrimitive(Aead::class.java)
+        val ciphertext = aead.encrypt(data, null)
+        return ciphertext
+    }
+
     // Construir y guardar el archivo con los datos recibidos
     private fun saveFile(context: Context, data: ByteArray, fileName: String, bytes: Int){
         // Obtener el directorio de almacenamiento externo de la aplicación
