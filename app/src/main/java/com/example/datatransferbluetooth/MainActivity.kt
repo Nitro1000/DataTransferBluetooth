fc3f1b55624540cc3af027da1dd8678d6fc2da10
Nitro1000 <47580067+Nitro1000@users.noreply.github.com>
2023-07-16 20:02:03 +0100

    Cambio de icono y permisos

--- app/src/main/java/com/example/datatransferbluetooth/MainActivity.kt
+++ app/src/main/java/com/example/datatransferbluetooth/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.datatransferbluetooth
 
+import android.app.AlertDialog
 import android.bluetooth.BluetoothAdapter
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -20,6 +23,12 @@ import com.example.datatransferbluetooth.presentation.MainScreen
 import com.example.datatransferbluetooth.presentation.ServerScreen
 
 class MainActivity : ComponentActivity() {
+
+    private val permissionRequestCode = 200
+    val requiredPermissions = arrayOf(
+        android.Manifest.permission.BLUETOOTH_CONNECT,
+        android.Manifest.permission.BLUETOOTH_SCAN
+    )
 
     private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 
@@ -78,6 +87,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Aquí verificamos si tenemos los permisos.
+        if (!hasPermissions(requiredPermissions)) {
+            // Si no los tenemos, los solicitamos.
+            ActivityCompat.requestPermissions(this, requiredPermissions, permissionRequestCode)
+        }
+
         checkBluetoothAndRequestEnable()
         registerBluetoothStateReceiver()
 
@@ -127,7 +142,7 @@ class MainActivity : ComponentActivity() {
                     MainScreen(
                         navController = navController,
                         bluetoothController = bluetoothController
-                        )
+                    )
                 }
                 composable(route = "pairedDevices"){
                     BluetoothDeviceListScreen(
@@ -159,6 +174,35 @@ class MainActivity : ComponentActivity() {
                         bluetoothController = bluetoothController
                     )
                 }
+            }
+        }
+    }
+
+    // Método para verificar si los permisos están concedidos.
+    private fun hasPermissions(permissions: Array<String>): Boolean {
+        return permissions.all {
+            ActivityCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    // Método para manejar la respuesta del usuario a la solicitud de permisos.
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == permissionRequestCode) {
+            if (grantResults.any { it == PackageManager.PERMISSION_DENIED }) {
+                // Si el usuario niega uno o más permisos, manejamos esta situación
+                // (mostramos un diálogo, cerramos la aplicación, etc).
+                AlertDialog.Builder(this)
+                    .setTitle("Solicitud de permisos")
+                    .setMessage("¿Acepta conceder los permisos requeridos para Bluetooth?")
+                    .setPositiveButton("Aceptar") { _, _ -> requestPermissions(requiredPermissions, 0) }
+                    .setNegativeButton("Cancelar") { _, _ -> finish() }
+                    .create()
+                    .show()
             }
         }
     }
