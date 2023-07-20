3b8e05ddb240d1abde0aa6a7ca4367071be5a8d9
Nitro1000 <47580067+Nitro1000@users.noreply.github.com>
2023-07-20 18:27:31 +0100

    Coroutine

--- app/src/main/java/com/example/datatransferbluetooth/BluetoothController.kt
+++ app/src/main/java/com/example/datatransferbluetooth/BluetoothController.kt
@@ -10,6 +10,10 @@ import android.widget.Toast
 import com.google.crypto.tink.aead.AeadConfig
 import com.google.crypto.tink.subtle.AesGcmJce
 import com.example.datatransferbluetooth.Models.BluetoothDeviceModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -108,102 +112,115 @@ class BluetoothController(
 
     // método para iniciar el servidor Bluetooth.
     fun startBluetoothServer() {
-        bluetoothServerSocket = bluetoothAdapter?.listenUsingRfcommWithServiceRecord(
-            "BluetoothServer",
-            UUID.fromString(SERVICE_UUID)
-        )
+        CoroutineScope(Dispatchers.IO).launch {
+            bluetoothServerSocket = bluetoothAdapter?.listenUsingRfcommWithServiceRecord(
+                "BluetoothServer",
+                UUID.fromString(SERVICE_UUID)
+            )
 
-        var shouldLoop = true
-        while(shouldLoop){
-            try {
-                bluetoothServerSocket?.accept()?.let { socket ->
+            var shouldLoop = true
+            while (shouldLoop) {
+                try {
+                    bluetoothServerSocket?.accept()?.let { socket ->
 
-                    bluetoothServerSocket?.close()
-                    bluetoothServerSocket = null
-                    bluetoothClientSocket = socket
-                    bluetoothClientSocket?.outputStream?.apply {
-                        val clientPublicKeySizeBuffer = ByteArray(4)
-                        bluetoothClientSocket?.inputStream?.read(clientPublicKeySizeBuffer)
+                        bluetoothServerSocket?.close()
+                        bluetoothServerSocket = null
+                        bluetoothClientSocket = socket
+                        bluetoothClientSocket?.outputStream?.apply {
+                            val clientPublicKeySizeBuffer = ByteArray(4)
+                            bluetoothClientSocket?.inputStream?.read(clientPublicKeySizeBuffer)
 
-                        val clientPublicKeyBuffer = ByteArray(ByteBuffer.wrap(clientPublicKeySizeBuffer).int)
-                        bluetoothClientSocket?.inputStream?.read(clientPublicKeyBuffer)
+                            val clientPublicKeyBuffer =
+                                ByteArray(ByteBuffer.wrap(clientPublicKeySizeBuffer).int)
+                            bluetoothClientSocket?.inputStream?.read(clientPublicKeyBuffer)
 
-                        val keyFactory = KeyFactory.getInstance("EC")
-                        val clientPublicKeySpec = X509EncodedKeySpec(clientPublicKeyBuffer)
-                        clientPublicKey = keyFactory.generatePublic(clientPublicKeySpec)
+                            val keyFactory = KeyFactory.getInstance("EC")
+                            val clientPublicKeySpec = X509EncodedKeySpec(clientPublicKeyBuffer)
+                            clientPublicKey = keyFactory.generatePublic(clientPublicKeySpec)
 
-                        val serverKeyAgreement: KeyAgreement = KeyAgreement.getInstance("ECDH")
-                        serverKeyAgreement.init(serverPrivateKey)
-                        serverKeyAgreement.doPhase(clientPublicKey, true)
+                            val serverKeyAgreement: KeyAgreement = KeyAgreement.getInstance("ECDH")
+                            serverKeyAgreement.init(serverPrivateKey)
+                            serverKeyAgreement.doPhase(clientPublicKey, true)
 
-                        sharedSecretserver= serverKeyAgreement.generateSecret()
+                            sharedSecretserver = serverKeyAgreement.generateSecret()
 
-                        val serverPublicKeySizeBuffer = ByteBuffer.allocate(4).putInt(serverPublicKey.encoded.size).array()
-                        bluetoothClientSocket?.outputStream?.write(serverPublicKeySizeBuffer)
+                            val serverPublicKeySizeBuffer =
+                                ByteBuffer.allocate(4).putInt(serverPublicKey.encoded.size).array()
+                            bluetoothClientSocket?.outputStream?.write(serverPublicKeySizeBuffer)
 
-                        bluetoothClientSocket?.outputStream?.write(serverPublicKey.encoded)
+                            bluetoothClientSocket?.outputStream?.write(serverPublicKey.encoded)
 
-                        val serverSharedSecret128 = sharedSecretserver?.sliceArray(0 until 16)
+                            val serverSharedSecret128 = sharedSecretserver?.sliceArray(0 until 16)
 
-                        aead = AesGcmJce(serverSharedSecret128)
+                            aead = AesGcmJce(serverSharedSecret128)
 
 
-                    }
-                    shouldLoop = false
-                    Toast.makeText(context, "Conexión con cliente establecida", Toast.LENGTH_SHORT).show()
-                }
-
-                // Hilo para escuchar los mensajes entrantes del cliente
-                Thread {
-                    while (true) {
-                        try {
-                            // Leer el tamaño nombre del archivo
-                            val fileNameSizeBuffer = ByteArray(4)
-                            bluetoothClientSocket?.inputStream?.read(fileNameSizeBuffer)
-                            val fileNameSize = ByteBuffer.wrap(fileNameSizeBuffer).int
-                            // Leer el nombre del archivo
-                            val fileNameBuffer = ByteArray(fileNameSize)
-                            bluetoothClientSocket?.inputStream?.read(fileNameBuffer)
-                            val fileName = String(fileNameBuffer)
-
-                            // Leer el tamaño del archivo
-                            val fileSizeBuffer = ByteArray(4)
-                            bluetoothClientSocket?.inputStream?.read(fileSizeBuffer)
-                            val fileSize = ByteBuffer.wrap(fileSizeBuffer).int
-                            var bytes: Int
-                            var remainingBytes = fileSize
-                            val allFileData: ArrayList<Byte> = ArrayList()
-                            val fileBuffer = ByteArray(size = BUFFER_SIZE)
-                            while (remainingBytes > 0){
-                                bytes = bluetoothClientSocket?.inputStream?.read(fileBuffer,0,min(remainingBytes,BUFFER_SIZE)) ?: -1
-
-                                if (bytes == -1) {
-                                    break
-                                }
-                                remainingBytes -= bytes
-                                // Guardar los datos recibidos
-                                for (i in 0 until bytes) {
-                                    allFileData.add(fileBuffer[i])
-                                }
-                            }
-                            // Cuando ya no queda nada por leer, es momento de descifrar los datos.
-                            val fileDataAsArray = allFileData.toByteArray()
-                            val plaintext = aead?.decrypt(fileDataAsArray, null)
-
-                            if (plaintext != null) {
-                                saveFile(context, plaintext, fileName)
-                            } else {
-                                Log.e("Bluetooth", "Error al desencriptar los datos en startBluetoothServer")
-                            }
-                        } catch (e: IOException) {
-                            // Error al leer los datos
-                            Log.e("Bluetooth", "Error al leer los datos en startBluetoothServer", e)
-                            break
+                        }
+                        shouldLoop = false
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(context, "Conexión con cliente establecida", Toast.LENGTH_SHORT).show()
                         }
                     }
-                }.start()
-            }catch (e: IOException){
-                shouldLoop = false
+
+                    // Hilo para escuchar los mensajes entrantes del cliente
+                    Thread {
+                        while (true) {
+                            try {
+                                // Leer el tamaño nombre del archivo
+                                val fileNameSizeBuffer = ByteArray(4)
+                                bluetoothClientSocket?.inputStream?.read(fileNameSizeBuffer)
+                                val fileNameSize = ByteBuffer.wrap(fileNameSizeBuffer).int
+                                // Leer el nombre del archivo
+                                val fileNameBuffer = ByteArray(fileNameSize)
+                                bluetoothClientSocket?.inputStream?.read(fileNameBuffer)
+                                val fileName = String(fileNameBuffer)
+
+                                // Leer el tamaño del archivo
+                                val fileSizeBuffer = ByteArray(4)
+                                bluetoothClientSocket?.inputStream?.read(fileSizeBuffer)
+                                val fileSize = ByteBuffer.wrap(fileSizeBuffer).int
+                                var bytes: Int
+                                var remainingBytes = fileSize
+                                val allFileData: ArrayList<Byte> = ArrayList()
+                                val fileBuffer = ByteArray(size = BUFFER_SIZE)
+                                while (remainingBytes > 0) {
+                                    bytes = bluetoothClientSocket?.inputStream?.read(
+                                        fileBuffer,
+                                        0,
+                                        min(remainingBytes, BUFFER_SIZE)
+                                    ) ?: -1
+
+                                    if (bytes == -1) {
+                                        break
+                                    }
+                                    remainingBytes -= bytes
+                                    // Guardar los datos recibidos
+                                    for (i in 0 until bytes) {
+                                        allFileData.add(fileBuffer[i])
+                                    }
+                                }
+                                // Cuando ya no queda nada por leer, es momento de descifrar los datos.
+                                val fileDataAsArray = allFileData.toByteArray()
+                                val plaintext = aead?.decrypt(fileDataAsArray, null)
+
+                                if (plaintext != null) {
+                                    saveFile(context, plaintext, fileName)
+                                } else {
+                                    Log.e(
+                                        "Bluetooth",
+                                        "Error al desencriptar los datos en startBluetoothServer"
+                                    )
+                                }
+                            } catch (e: IOException) {
+                                // Error al leer los datos
+                                Log.e("Bluetooth", "Error al leer los datos en startBluetoothServer", e)
+                                break
+                            }
+                        }
+                    }.start()
+                } catch (e: IOException) {
+                    shouldLoop = false
+                }
             }
         }
     }
@@ -213,50 +230,56 @@ class BluetoothController(
         device: BluetoothDeviceModel,
         connectionListener: BluetoothConnectionListener
     ) {
-        bluetoothClientSocket = bluetoothAdapter
-            ?.getRemoteDevice(device.address)
-            ?.createRfcommSocketToServiceRecord(
-                UUID.fromString(SERVICE_UUID)
-            )
-        bluetoothClientSocket?.let { socket ->
-            try {
-                socket.connect()
+        CoroutineScope(Dispatchers.IO).launch {
+            bluetoothClientSocket = bluetoothAdapter
+                ?.getRemoteDevice(device.address)
+                ?.createRfcommSocketToServiceRecord(
+                    UUID.fromString(SERVICE_UUID)
+                )
+            bluetoothClientSocket?.let { socket ->
+                try {
+                    socket.connect()
 
 
-                val clientPublicKeySizeBuffer = ByteBuffer.allocate(4).putInt(clientPublicKey.encoded?.size ?: 0).array()
+                    val clientPublicKeySizeBuffer = ByteBuffer.allocate(4).putInt(clientPublicKey.encoded?.size ?: 0).array()
 
-                val clientPublicKeyBuffer = clientPublicKey.encoded ?: ByteArray(0)
+                    val clientPublicKeyBuffer = clientPublicKey.encoded ?: ByteArray(0)
 
-                socket.outputStream.write(clientPublicKeySizeBuffer)
+                    socket.outputStream.write(clientPublicKeySizeBuffer)
 
-                socket.outputStream.write(clientPublicKeyBuffer)
+                    socket.outputStream.write(clientPublicKeyBuffer)
 
 
-                val serverPublicKeySizeBuffer = ByteArray(4)
-                socket.inputStream.read(serverPublicKeySizeBuffer)
+                    val serverPublicKeySizeBuffer = ByteArray(4)
+                    socket.inputStream.read(serverPublicKeySizeBuffer)
 
-                val serverPublicKeySize = ByteBuffer.wrap(serverPublicKeySizeBuffer).int
-                val serverPublicKeyBuffer = ByteArray(serverPublicKeySize)
-                socket.inputStream.read(serverPublicKeyBuffer)
+                    val serverPublicKeySize = ByteBuffer.wrap(serverPublicKeySizeBuffer).int
+                    val serverPublicKeyBuffer = ByteArray(serverPublicKeySize)
+                    socket.inputStream.read(serverPublicKeyBuffer)
 
-                val keyFactory = KeyFactory.getInstance("EC")
-                val serverPublicKeySpec = X509EncodedKeySpec(serverPublicKeyBuffer)
-                serverPublicKey = keyFactory.generatePublic(serverPublicKeySpec)
+                    val keyFactory = KeyFactory.getInstance("EC")
+                    val serverPublicKeySpec = X509EncodedKeySpec(serverPublicKeyBuffer)
+                    serverPublicKey = keyFactory.generatePublic(serverPublicKeySpec)
 
-                val clientKeyAgreement: KeyAgreement = KeyAgreement.getInstance("ECDH")
-                clientKeyAgreement.init(clientPrivateKey)
-                clientKeyAgreement.doPhase(serverPublicKey, true)
+                    val clientKeyAgreement: KeyAgreement = KeyAgreement.getInstance("ECDH")
+                    clientKeyAgreement.init(clientPrivateKey)
+                    clientKeyAgreement.doPhase(serverPublicKey, true)
 
-                sharedSecretclient = clientKeyAgreement.generateSecret()
-                val clientSharedSecret128 = sharedSecretclient?.sliceArray(0 until 16)
+                    sharedSecretclient = clientKeyAgreement.generateSecret()
+                    val clientSharedSecret128 = sharedSecretclient?.sliceArray(0 until 16)
 
-                aead = AesGcmJce(clientSharedSecret128)
+                    aead = AesGcmJce(clientSharedSecret128)
 
-                connectionListener.onConnectionSuccess()
-            } catch (e: IOException) {
-                socket.close()
-                bluetoothClientSocket = null
-                connectionListener.onConnectionError()
+                    withContext(Dispatchers.Main) {
+                        connectionListener.onConnectionSuccess()
+                    }
+                } catch (e: IOException) {
+                    socket.close()
+                    bluetoothClientSocket = null
+                    withContext(Dispatchers.Main) {
+                        connectionListener.onConnectionError()
+                    }
+                }
             }
         }
     }
